3f0ebdbdf5d7cac96f75987cff9b1f2076cfa76e
Marcus Quigley <marcusquigley@gmail.com>
2022-10-03 20:13:33 +0100

    adding status code and testing for non user

--- server_test.go
+++ server_test.go
@@ -18,6 +18,7 @@ func (s *StubPlayerStore) GetPlayerScore(name string) int {
 }
 
 func TestGETPlayers(t *testing.T) {
+
 	store := StubPlayerStore{
 		map[string]int{
 			"Pepper": 20,
@@ -29,19 +30,35 @@ func TestGETPlayers(t *testing.T) {
 		request := newGetScoreRequest("Pepper")
 		response := httptest.NewRecorder()
 		server.ServeHTTP(response, request)
+		assertStatus(t, response.Code, http.StatusOK)
 		assertResponseBody(t, response.Body.String(), "20")
 	})
 	t.Run("return Floyds score", func(t *testing.T) {
 		request := newGetScoreRequest("Floyd")
 		response := httptest.NewRecorder()
 		server.ServeHTTP(response, request)
+		assertStatus(t, response.Code, http.StatusOK)
 		assertResponseBody(t, response.Body.String(), "200")
+	})
+
+	t.Run("returns 404 on missing player", func(t *testing.T) {
+		request := newGetScoreRequest("Apollo")
+		response := httptest.NewRecorder()
+		server.ServeHTTP(response, request)
+		assertStatus(t, response.Code, http.StatusNotFound)
+
 	})
 }
 
 func newGetScoreRequest(name string) *http.Request {
 	req, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/players/%s", name), nil)
 	return req
+}
+
+func assertStatus(t testing.TB, got, want int) {
+	if got != want {
+		t.Errorf("got %d but want %d", got, want)
+	}
 }
 
 func assertResponseBody(t testing.TB, got, want string) {

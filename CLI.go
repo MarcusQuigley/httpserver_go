9b7214427988e96de847dc3e7446253a450f849c
Marcus Quigley <marcusquigley@gmail.com>
2022-10-26 12:18:49 -0400

    adding playing poker to record wins

--- CLI.go
+++ CLI.go
@@ -1,12 +1,33 @@
 package poker
 
-import "io"
+import (
+	"bufio"
+	"errors"
+	"io"
+	"strings"
+)
 
 type CLI struct {
 	playerStore PlayerStore
 	in          io.Reader
 }
 
-func (cli *CLI) PlayPoker() {
-	cli.playerStore.RecordWin("Joe")
+func (cli *CLI) PlayPoker() error {
+	reader := bufio.NewScanner(cli.in)
+	reader.Scan()
+	player, err := extractWinner(reader.Text())
+	if err != nil {
+		return err
+	}
+	cli.playerStore.RecordWin(player)
+	return nil
+}
+
+func extractWinner(input string) (string, error) {
+	var stringToReplace = " wins"
+	if len(input) == 0 {
+		return "", errors.New("no player name exists")
+	}
+	player := strings.Replace(input, stringToReplace, "", 1)
+	return player, nil
 }
--- CLI_test.go
+++ CLI_test.go
@@ -6,10 +6,20 @@ import (
 )
 
 func TestCLI(t *testing.T) {
-	in := strings.NewReader("Joe wins\n")
-	playerStore := &StubPlayerStore{}
-	cli := &CLI{playerStore, in}
-	cli.PlayPoker()
+	t.Run("record Joe win from user input", func(t *testing.T) {
+		in := strings.NewReader("Joe wins\n")
+		playerStore := &StubPlayerStore{}
+		cli := &CLI{playerStore, in}
+		cli.PlayPoker()
 
-	assertPlayerWins(t, playerStore, "Joe")
+		assertPlayerWins(t, playerStore, "Joe")
+	})
+	t.Run("record chris win from user input", func(t *testing.T) {
+		in := strings.NewReader("Chris wins\n")
+		playerStore := &StubPlayerStore{}
+		cli := &CLI{playerStore, in}
+		cli.PlayPoker()
+
+		assertPlayerWins(t, playerStore, "Chris")
+	})
 }
